4a26d21e22eeed703fd498cf37dffb4cb1a27d4e
Maciej Przybylski <macio2013@outlook.com>
2019-08-14 16:56:53 +0200

    Initial Commit

---
+++ Maths/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maths
+//
+//  Created by Maciej Przybylski on 14/08/2019.
+//  Copyright © 2019 Maciej Przybylski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
